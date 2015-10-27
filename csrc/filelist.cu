9eec776d2191c6e836df50fa1176e1867d44d88a
Edward Shin <edshin95@gmail.com>
2015-10-27 04:40:42 -0400

    prelab 100 percent

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,12 +1,20 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _69996_archive_1.so
-_69996_archive_1.so : archive.0/_69996_archive_1.a
+ARCHIVE_OBJS += _141607_archive_1.so
+_141607_archive_1.so : archive.116/_141607_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_69996_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_141607_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_69996_archive_1.so $@
+	@ln -sf .//../simv.daidir//_141607_archive_1.so $@
+
+
+ARCHIVE_OBJS += _prev_archive_1.so
+_prev_archive_1.so : archive.116/_prev_archive_1.a
+	@$(AR) -s $<
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_prev_archive_1.so --whole-archive $< --no-whole-archive
+	@rm -f $@
+	@ln -sf .//../simv.daidir//_prev_archive_1.so $@
 
 
 
--- usbHost.sv
+++ usbHost.sv
@@ -9,10 +9,28 @@ module usbHost
   /* Tasks needed to be finished to run testbenches */
   logic [87:0] packet;
 
+  busState enc_busState, bs_outputBusState, nrzi_outputBusState;
+
+  logic enc_dataReady, enc_okToSend;
+  logic bs_dataReady, bs_stuffEnable;
+  logic nrzi_dataReady, nrzi_outputValid;
+
+
+  assign wires.DP = nrzi_outputValid ? (nrzi_outputBusState == bus_J) : 1'bz;
+  assign wires.DM = nrzi_outputValid ? (nrzi_outputBusState == bus_K) : 1'bz;
 
   task prelabRequest();
   // sends an OUT packet with ADDR=5 and ENDP=4
   // packet should have SYNC and EOP too
+    //$monitor("clk(%x) enc.state(%s) enc.index(%d) enc.pid(%s) enc_busState(%s) bs_dataReady(%x) enc_okToSend(%x) nrzi_outputBusState(%s)", clk, enc0.state, enc0.index,  enc0.pid, enc_busState, bs_dataReady, enc_okToSend, nrzi_outputBusState);
+    //$monitor("clk(%x) bs.counter(%d) bs_dataReady(%x) enc_okToSend(%x) nrzi_dataReady(%x) enc_busState(%x) bs_outputBusState(%x) nrzi_outputBusState(%s)", clk, bs0.counter, bs_dataReady, enc_okToSend, nrzi_dataReady, enc_busState, bs_outputBusState, nrzi_outputBusState);
+    //$monitor("clk(%x) nrzi_dataReady(%x) nrzi_outputBusState(%s) nrzi_outputValid(%x) DP(%x) DM(%x)", clk, nrzi_dataReady, nrzi_outputBusState, nrzi_outputValid, wires.DP, wires.DM);
+    //$monitor("crc5.state(%s) crc.counter(%d) crc5.dataReg(%b) crc5.crc(%b) crc5.crc0_next(%b) crc5.crc2_next(%b)", enc0.a1.state, enc0.a1.counter, enc0.a1.dataReg, enc0.a1.crc, enc0.a1.crc0_next, enc0.a1.crc2_next);
+    packet <= {4'd4,7'd5,~4'd1,4'd1};
+    enc_dataReady <= 0;
+    #10 @(posedge clk) enc_dataReady <= 1;
+    @(posedge clk) enc_dataReady <= 0;
+    repeat(100)@(posedge clk) packet = 0;
 
   endtask: prelabRequest
 
@@ -34,17 +52,11 @@ module usbHost
 
   endtask: writeData
 
-  busState enc_busState, bs_outputBusState, nrzi_outputBusState;
+  encoder enc0(clk, rst_L, enc_dataReady, enc_okToSend, packet, enc_busState, bs_dataReady, bs_stuffEnable);
 
-  logic enc_dataReady, enc_okToSend;
-  logic bs_dataReady, bs_okToSend;
-  logic nrzi_dataReady;
+  bitStuff bs0(clk, rst_L, bs_dataReady, bs_stuffEnable, enc_busState, enc_okToSend, nrzi_dataReady, bs_outputBusState);
 
-  encoder enc0(clk, rst_L, enc_dataReady, enc_okToSend, packet, enc_busState, bs_dataReady);
-
-  bitStuff bs0(clk, rst_L, bs_dataReady, bs_okToSend, enc_busState, enc_okToSend, nrzi_dataReady, bs_outputBusState);
-
-  nrzi nrzi0(clk, rst_L, nrzi_dataReady, bs_outputBusState, nrzi_outputBusState);
+  nrzi nrzi0(clk, rst_L, nrzi_dataReady, bs_outputBusState, nrzi_outputValid, nrzi_outputBusState);
 
 endmodule: usbHost
 
@@ -57,7 +69,7 @@ module encoder
     (input logic clk, rst_L, dataReady, okToSend,
      input logic [87:0] packet,
      output busState outputBusState,
-     output logic outputReady);
+     output logic outputReady, stuffEnable);
 
     logic [7:0] index; //Index counter
     logic [87:0] inputReg; //Holds our data
@@ -66,16 +78,19 @@ module encoder
     logic [4:0] crc5Result;
 	   
     enum {WAIT, SYNC, DATA, CRC, EOP} state, nextState;
-    enum logic[3:0] {OUT = 4'b0001, IN = 4'b1001, DATA0 = 4'b0011,
-                     ACK = 4'b0010, NAK = 4'b1010} pid;
+    typedef enum logic[3:0] {OUT = 4'b0001, IN = 4'b1001, DATA0 = 4'b0011,
+                     ACK = 4'b0010, NAK = 4'b1010} pidValue;
+    pidValue pid;
 
     crc5 a1(clk, rst_L, packet[18:8], dataReady, crc5Result);
     crc16 a2(clk, rst_L, packet[71:8], dataReady, crc16Result);
    
     always_comb begin
-        crc = (pid == DATA0) ? crc16Result : crc5Result;
+        crc = (pid == DATA0) ? ~crc16Result : ~crc5Result;
         outputBusState = bus_J;
-        pid = inputReg[3:0];
+        outputReady = (state != WAIT);
+        pid = pidValue'(inputReg[3:0]);
+        stuffEnable = 0;
         case(state)
             // WAIT: Stall until we receive a packet
             WAIT: begin
@@ -94,7 +109,8 @@ module encoder
             end
             // DATA: Increment a counter to transmit the packet payload one bit at a time
             DATA: begin
-                outputBusState = inputReg[index];
+                outputBusState = busState'(inputReg[index]);
+                stuffEnable = index >= 8;
                 nextState = DATA;
                 case(pid)
                     OUT: begin
@@ -116,9 +132,9 @@ module encoder
             end
             // CRC: Output the CRC of the payload 
             CRC: begin
-                outputBusState = crc[counter];
-                if(((pid == OUT || pid == IN) && (counter == 8'd4)) ||
-                   (pid == DATA0 && counter == 8'd15)) begin
+                stuffEnable = 1;
+                outputBusState = busState'(crc[counter]);
+                if(counter == 0) begin
                     nextState = EOP;
                 end
                 else nextState = CRC;
@@ -159,15 +175,19 @@ module encoder
             end
             if(state == DATA) begin
                 index <= index + 1;
-            end
-            if(state == CRC) begin
-                counter <= counter + 1;
-                if(nextState == EOP) begin
-                    counter <= 0;
+                if(nextState == CRC) begin
+                    if(pid == DATA0) counter = 15;
+                    else counter = 4;
                 end
             end
-            if(state == EOP && nextState == WAIT) begin
+            if(state == CRC) begin
+                counter <= counter - 1;
+            end
+            if(nextState == EOP) begin
                 counter <= 0;
+            end
+            if(state == EOP) begin
+                counter <= counter + 1;
             end
             state <= nextState;
         end
@@ -186,31 +206,35 @@ module crc5
     logic [10:0] dataReg;
     logic [4:0] counter;
 
+    logic crc0_next, crc2_next;
+
     always_comb begin
         case(state)
             WAIT: nextState = (dataReady) ? GO : WAIT;
             GO: nextState = (counter == 5'd10) ? WAIT : GO;
         endcase
+
+        crc0_next = dataReg[counter]^crc[4];
+        crc2_next = (crc0_next^crc[1]);
     end
    
     always_ff @(posedge clk, negedge rst_L) begin
         if(~rst_L) begin
             state <= WAIT;
-            crc <= 0;
+            crc <= 5'h1f;
             counter <= 0;
         end
         else begin
             if(state == WAIT && nextState == GO) begin
                 dataReg <= data;
-                crc <= 0;
+                crc <= 5'h1f;
                 counter <= 0;
             end
             if(state == GO) begin
-                crc[0] <= (dataReg[counter]^crc[4]);
+                crc[0] <= crc0_next;
                 crc[1] <= crc[0];
-                crc[2] <= (dataReg[counter]^crc[4])^crc[1];
-                crc[3] <= crc[2];
-                crc[4] <= crc[3];
+                crc[2] <= crc2_next;
+                crc[4:3] <= crc[3:2];
                 counter <= counter + 1;
             end
             state <= nextState;
@@ -221,20 +245,20 @@ endmodule : crc5
 
 module crc16
     (input logic clk, rst_L,
-     input logic [71:0]  data,
+     input logic [63:0]  data,
      input logic dataReady,
      output logic [15:0] crc);
 
    
     enum {WAIT, GO} state, nextState;
 
-    logic [71:0] dataReg;
+    logic [63:0] dataReg;
     logic [7:0] counter;
 
     always_comb begin
         case(state)
             WAIT: nextState = (dataReady) ? GO : WAIT;
-            GO: nextState = (counter == 8'd71) ? WAIT : GO;
+            GO: nextState = (counter == 8'd63) ? WAIT : GO;
         endcase
     end
 
@@ -265,16 +289,15 @@ module crc16
 endmodule : crc16
 
 module bitStuff
-    (input logic clk, rst_L, dataReady, okToSend,
-     input inputBusState,
+    (input logic clk, rst_L, dataReady, stuffEnable,
+     input busState inputBusState,
      output logic readyToReceive, outputReady,
-     output outputBusState);
+     output busState outputBusState);
 
-    busState inputBusState, outputBusState;
-   
     logic [2:0]	counter;
 
     always_comb begin
+        outputReady = dataReady || (counter == 6);
         outputBusState = (counter == 6) ? bus_K : inputBusState;   
         readyToReceive = !(counter == 5);
     end
@@ -288,7 +311,7 @@ module bitStuff
                 counter <= 0;
             end
             else if(dataReady) begin
-                if(inputBusState == bus_J) counter <= counter + 1;
+                if(stuffEnable && inputBusState == bus_J) counter <= counter + 1;
                 else counter <= 0;
             end
         end
@@ -298,21 +321,23 @@ endmodule : bitStuff
 
 module nrzi
     (input logic clk, rst_L, dataReady,
-     input inputBusState,
-     output outputBusState);
-
-    busState inputBusState, outputBusState;
+     input busState inputBusState,
+     output logic outputValid,
+     output busState outputBusState);
 
     always_ff @(posedge clk, negedge rst_L) begin
         if(~rst_L) begin
             outputBusState <= bus_J;
+            outputValid <= 0;
         end
         else begin
             if(dataReady) begin
-                if(inputBusState == bus_K) outputBusState <= (outputBusState == bus_K) ? bus_J : bus_K;
+                outputValid <= 1;
+                if(outputBusState == bus_SE0) outputBusState <= inputBusState;
+                else if(inputBusState == bus_K) outputBusState <= (outputBusState == bus_K) ? bus_J : bus_K;
                 else if(inputBusState == bus_SE0) outputBusState <= bus_SE0;
-                else outputBusState <= inputBusState;
             end
+            else outputValid <= 0;
         end
     end
 endmodule : nrzi
