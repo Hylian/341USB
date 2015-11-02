1bafd7dda4bc9858cc3a8953d7d54e44c201c27c
Edward Shin <edshin95@gmail.com>
2015-11-02 14:12:18 -0500

    fixed .gitignore

--- .synopsys_dve_rebuild.tcl
+++
@@ -1,9 +0,0 @@
-# DVE Simulation Rebuild/Restart Options
-# Saved on Mon Nov 2 12:30:15 2015
-set SIMSETUP::REBUILDOPTION 1
-set SIMSETUP::REBUILDCMD {vcs -sverilog -debug top.sv usbBusAnalyzer.svp TA_tb_simple.svp usbHost.sv thumb.svp}
-set SIMSETUP::REBUILDDIR {}
-set SIMSETUP::RESTOREBP 1
-set SIMSETUP::RESTOREDUMP 1
-set SIMSETUP::RESTOREFORCE 1
-set SIMSETUP::RESTORESPECMAN 0
